ef3dec6b98d22c637792b378ff266970670e8882
Jasper Robison <jasper.robison22@gmail.com>
2022-06-02 19:38:18 +1000

    Conversion

---
+++ user/options/init.lua
@@ -0,0 +1,34 @@
+-- map leader
+vim.g.mapleader = ','
+-- vim commands
+-- -- main neovim settings
+vim.o.termguicolors = true
+vim.o.syntax = 'on'
+-- o.errorbells = false
+-- o.showmode = false
+-- bo.swapfile = false
+-- o.backup = false
+-- o.undofile = false
+-- o.incsearch = true
+-- o.hidden = true
+-- -- autocomplete options
+-- o.completeopt = 'menuone,noinsert,noselect'
+-- -- ignore case unless capital in search
+-- o.ignorecase = true
+-- o.smartcase = true
+-- -- indentation
+-- bo.autoindent = true
+-- bo.smartindent = true
+-- o.tabstop = 8
+-- o.softtabstop = 2
+-- o.shiftwidth = 2
+-- o.expandtab = true
+-- -- enable cursorline
+-- wo.cursorline = true
+-- -- display line numbers
+-- wo.number = true
+-- wo.signcolumn = 'yes'
+-- wo.wrap = true
+
+-- set colorscheme
+vim.g.colors_name = 'nightfox'
---
+++ user/plugins/init.lua
@@ -0,0 +1,102 @@
+--- install packer if unable to load
+if not pcall(require, 'packer') then
+	local packer_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+	vim.cmd('!git clone https://github.com/wbthomason/packer.nvim ' .. packer_path)
+	print 'packer.vim installed ... please restart neovim'
+end
+
+-- init plugin manager
+return require('packer').startup(function(use)
+
+	-- packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+	-- sidebar
+	use 'preservim/nerdtree'
+
+	-- git sidebar status
+	use 'lewis6991/gitsigns.nvim'
+
+	-- buffer tabline
+	use {
+		'akinsho/bufferline.nvim',
+		requires = {
+			'kyazdani42/nvim-web-devicons',
+			opt = true,
+		},
+	}
+
+	-- status line
+	use {
+		'hoob3rt/lualine.nvim',
+		requires = {
+			'kyazdani42/nvim-web-devicons',
+			opt = true,
+		},
+	}
+
+	-- fzf native (telescope extension)
+	use {
+		'nvim-telescope/telescope-fzf-native.nvim',
+		run = 'make',
+	}
+
+	---- lsp
+	use 'neovim/nvim-lspconfig'
+
+	-- Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
+	-- Plug 'junegunn/fzf.vim'
+	-- Plug 'puremourning/vimspector'
+	-- "
+	-- "  Quality Of Life
+	-- Plug 'tpope/vim-eunuch'
+	-- Plug 'szw/vim-maximizer'
+	-- Plug 'jesseleite/vim-agriculture'
+	-- Plug 'tpope/vim-fugitive'
+	-- Plug 'tpope/vim-surround'
+	-- Plug 'tpope/vim-commentary'
+	-- Plug 'uiiaoo/java-syntax.vim'
+	-- Plug 'pangloss/vim-javascript'
+	-- Plug 'sheerun/vim-polyglot'
+	-- Plug 'junegunn/rainbow_parentheses.vim'
+	-- Plug 'ryanoasis/vim-devicons'
+	-- Plug 'hoob3rt/lualine.nvim'
+
+
+	---- completion
+	-- use {
+	--   'hrsh7th/nvim-cmp',
+	--   requires = {
+	--     'hrsh7th/cmp-buffer',
+	--     'hrsh7th/cmp-path',
+	--     'hrsh7th/cmp-nvim-lua',
+	--     'hrsh7th/cmp-nvim-lsp',
+	--   },
+	-- }
+
+	---- snippets
+	-- use 'rafamadriz/friendly-snippets'
+	-- use 'L3MON4D3/LuaSnip'
+	-- use 'saadparwaiz1/cmp_luasnip'
+
+	---- display indentation lines
+	-- use 'lukas-reineke/indent-blankline.nvim'
+
+	---- helpers / misc plugins
+	-- use 'jiangmiao/auto-pairs'
+	-- use 'tpope/vim-commentary'
+	-- use 'tpope/vim-surround'
+
+	---- colorschemes
+	-- main colorscheme
+	use 'bluz71/vim-nightfly-guicolors'
+	use 'tomasiser/vim-code-dark'
+	use 'patstockwell/vim-monokai-tasty'
+	use 'ErichDonGubler/vim-sublime-monokai'
+	use 'crusoexia/vim-monokai'
+	use 'EdenEast/nightfox.nvim'
+	use 'junegunn/seoul256.vim'
+
+	-- vimdiff colorscheme
+	-- use 'nanotech/jellybeans.vim'
+end)
