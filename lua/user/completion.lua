972f11fd72d951f61148985463be8fe3dd6079a4
Jasper Robison <jasper.robison22@gmail.com>
2022-06-05 10:51:15 +1000

    tab selection for completion

--- lua/user/completion.lua
+++ lua/user/completion.lua
@@ -20,6 +20,22 @@ local cmp = require'cmp'
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
       ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ['<Tab>'] = cmp.mapping(function(fallback)
+		  if cmp.visible() then
+			  cmp.select_next_item()
+			  return
+		  end
+		  fallback()
+	  end
+	  ),
+      ['<S-Tab>'] = cmp.mapping(function(fallback)
+		  if cmp.visible() then
+			  cmp.select_prev_item()
+			  return
+		  end
+		  fallback()
+	  end
+	  ),
     }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
